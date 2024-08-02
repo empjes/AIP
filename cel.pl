7e690c85d1e00ec7dc407097f1dfa73918ae9596
EMPEROR <98907680+empjes@users.noreply.github.com>
2024-08-02 11:34:49 +0530

    cel.pl

--- cel.pl
+++ cel.pl
@@ -3,3 +3,7 @@ celsius_to_fahrenheit(Celcius,Fahrenheit):-
  Fahrenheit is(Celsius*9/5)+32.
 below_freezing(Fahrenheit):-
  Fahrenheit<32.
+
+output
+celsius_to_fahrenheit(98,Fahrenheit).
+below_freezing(20).
